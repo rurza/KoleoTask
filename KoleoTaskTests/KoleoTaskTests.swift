f62fd8668cf12df0068d3eac6651f8ca71927d5d
Adam Różyński <adam@micropixels.pl>
2017-06-09 21:39:30 +0200

    Initial Commit

---
+++ KoleoTaskTests/KoleoTaskTests.swift
@@ -0,0 +1,36 @@
+//
+//  KoleoTaskTests.swift
+//  KoleoTaskTests
+//
+//  Created by rurza on 09/06/2017.
+//  Copyright © 2017 Adam Różyński. All rights reserved.
+//
+
+import XCTest
+@testable import KoleoTask
+
+class KoleoTaskTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
